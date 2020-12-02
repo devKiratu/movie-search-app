9990b7be11cc9f363b38e6f1ccaefcf9c5fae727
devkiratu <dev.kiratu@gmail.com>
2020-12-02 12:49:33 +0300

    test deployment settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function App() {
-	return <h1>Hello from React. This is me</h1>;
+	return (
+		<h1>
+			Hello from React. This is me. I changed the text to test build on netlify
+		</h1>
+	);
 }
